5312008a78acb3678dc2f9e38c0919fd941d361b
a18lukto <a18lukto@student.his.se>
2019-04-16 15:58:04 +0200

    Toast med listan

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.brom.activitiesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
@@ -19,14 +25,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Lägger till berg 3st
         lukasBerg.add(new Mountain("Matterhorn", "Alps",4478));
         lukasBerg.add(new Mountain("Mont Blanc", "Alps", 4808));
         lukasBerg.add(new Mountain("Denali", "Alaska", 6190));
 
+        //Log för att kolla så det funka
         Log.d("a18luktoBerg",lukasBerg.get(0).getName());
 
+        ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this,R.layout.list_item_textview,R.id.my_item_textview, lukasBerg);
 
+        ListView my_listView=(ListView) findViewById(R.id.my_listview);
 
+        my_listView.setAdapter(adapter);
+
+        //Gör så att Toasten visar rätt information
+        my_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(), lukasBerg.get(position).info(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
