39a6da4854568b7d59b84879735c5e535eecef98
a18lukto <a18lukto@student.his.se>
2019-04-24 08:58:09 +0200

    Färdig

--- app/src/main/java/com/example/brom/activitiesapp/detaljer.java
+++ app/src/main/java/com/example/brom/activitiesapp/detaljer.java
@@ -2,12 +2,9 @@ package com.example.brom.activitiesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 public class detaljer extends AppCompatActivity {
@@ -22,17 +19,17 @@ public class detaljer extends AppCompatActivity {
         Intent intent = getIntent();
         String mountname = intent.getStringExtra(MainActivity.EXTRA_NAME);
         String mountlocation = intent.getStringExtra(MainActivity.EXTRA_LOCATION);
-        String mountheight = intent.getStringExtra(MainActivity.EXTRA_HEIGHT);
+        int mountheight = intent.getIntExtra(MainActivity.EXTRA_HEIGHT, -1);
         Log.d("a18lukto", "Fick följande från main activitt: "+mountname );
 
         TextView headerView = findViewById(R.id.textDetaljerHeader);
         headerView.setText(mountname);
 
-        TextView textView1 = findViewById(R.id.textDetaljerText);
-        textView1.setText(mountlocation);
+        TextView textView1 = findViewById(R.id.textDetaljerText2);
+        textView1.setText("Plats: "+mountlocation);
 
-        TextView textView2 = findViewById(R.id.textDetaljerText2);
-        textView2.setText(mountheight);
+        TextView textView2 = findViewById(R.id.textDetaljerText);
+        textView2.setText("Höjd: "+mountheight+"m");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
